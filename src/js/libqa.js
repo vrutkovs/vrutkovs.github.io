e23b3d49abb428a4241b1032fe49f0979637988c
Colin Walters <walters@verbum.org>
2013-04-05 16:34:36 -0400

    libqa: Set /boot partition to be active
    Since some bootloaders expect that.

--- src/js/libqa.js
+++ src/js/libqa.js
@@ -70,6 +70,7 @@ blockdev-getss /dev/sda\n';
 part-add /dev/sda p %s %s\n\
 part-add /dev/sda p %s %s\n\
 part-add /dev/sda p %s %s\n\
+part-set-bootable /dev/sda 1 true\n\
 mkfs ext4 /dev/sda1\n\
 set-e2label /dev/sda1 gnostree-boot\n\
 mkswap-L gnostree-swap /dev/sda2\n\
