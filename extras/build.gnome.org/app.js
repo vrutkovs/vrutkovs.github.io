71fa8a0db477b7b0822423ef7f95c24aaa507de0
Jasper St. Pierre <jstpierre@mecheye.net>
2013-10-14 12:35:55 -0400

    build.gnome.org: Rewrite using AngularJS
    I spent more time fighting jQuery Mobile than writing the website,
    so I figured that jQuery Mobile wasn't build for this use case.
    
    Through a dice roll, I picked AngularJS, and it seems to be working
    fairly well for me.

---
+++ extras/build.gnome.org/app.js
@@ -0,0 +1,26 @@
+(function(exports) {
+    'use strict';
+
+    var bgo = angular.module('build.gnome.org', [
+        'ngRoute',
+        'bgoControllers',
+    ]);
+
+    bgo.config(['$routeProvider', function($routeProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'partials/home.html'
+            }).
+            when('/jhbuild-ubuntu-raring', {
+                templateUrl: 'partials/jhbuild-ubuntu-raring.html'
+            }).
+            when('/gnome-continuous', {
+                templateUrl: 'partials/gnome-continuous.html',
+                controller: 'GnomeContinuousCtrl',
+            }).
+            otherwise({
+                redirectTo: '/',
+            });
+    }]);
+
+})(window);
---
+++ extras/build.gnome.org/controllers.js
@@ -0,0 +1,42 @@
+(function(exports) {
+    'use strict';
+
+    var bgoControllers = angular.module('bgoControllers', []);
+
+    var taskNames = ['build', 'smoketest', 'integrationtest', 'applicationstest'];
+
+    function getUrl(suffix) {
+        return window.location.protocol + '//' + window.location.host +
+            window.location.pathname + 'continuous/buildmaster/' + suffix;
+    }
+
+    bgoControllers.controller('GnomeContinuousCtrl', function($scope, $http) {
+        $scope.getUrl = getUrl;
+
+        $http.get(getUrl('autobuilder-status.json')).success(function(status) {
+            var text;
+            if (status.running.length > 0)
+                text = 'Running: ' + status.running.join(' ') + '; load=' + status.systemLoad[0];
+            else
+                text = 'Idle, awaiting commits';
+
+            $scope.runningState = text;
+        });
+
+        var tasks = [];
+        taskNames.forEach(function(taskName) {
+            $http.get(getUrl('results/tasks/' + taskName + '/' + taskName + '/meta.json')).success(function(data) {
+                // Mangle the data a bit so we can render it better
+                data['name'] = taskName;
+
+                tasks.push(data);
+
+                if (taskName == 'smoketest')
+                    $scope.smoketestImage = getUrl(data['path'] + '/work-gnome-continuous-x86_64-runtime/screenshot-final.png');
+            });
+        });
+
+        $scope.tasks = tasks;
+    });
+
+})(window);
