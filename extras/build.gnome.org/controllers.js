512aa260d511dae5f5970193ef2a4098e18059dc
Colin Walters <walters@verbum.org>
2013-11-23 17:31:48 -0500

    build.gnome.org: Rework front page to show current task status
    And have a bit more explanation around each task.

--- extras/build.gnome.org/controllers.js
+++ extras/build.gnome.org/controllers.js
@@ -28,6 +28,33 @@
         });
     });
 
+    function renderApps($http, path, callback) {
+        $http.get(ROOT + path + '/applicationstest/apps.json').success(function(data) {
+            var apps = data['apps'];
+
+            // Older builds use a different scheme for the
+            // applicationstest; just ignore them.
+            if (!apps.forEach)
+                return;
+
+            apps.forEach(function(app) {
+                // Mangle the data a bit
+
+                app.name = app.id; /* XXX */
+                app.status = (app.state == "success") ? 'good' : 'bad';
+
+                // XXX -- this should probably be in the template
+                if (app.icon)
+                    app.icon = ROOT + app.icon;
+                else
+                    app.icon = '/images/app-generic.png';
+
+                app.screenshot = ROOT + app.screenshot;
+            });
+	    callback(apps);
+        });
+    }
+
     bgoControllers.controller('ContinuousBuildViewCtrl', function($scope, $http, $routeParams) {
         var buildVersion = $routeParams.buildVersion;
         $scope.buildVersion = buildVersion;
@@ -51,43 +78,19 @@
         });
         $scope.tasks = tasks;
 
-        var apps = [];
-        $http.get(buildRoot + 'applicationstest/apps.json').success(function(data) {
-            var apps = data['apps'];
-
-            // Older builds use a different scheme for the
-            // applicationstest; just ignore them.
-            if (!apps.forEach)
-                return;
-
-            apps.forEach(function(app) {
-                // Mangle the data a bit
-
-                app.name = app.id; /* XXX */
-                app.status = (app.state == "success") ? 'good' : 'bad';
-
-                // XXX -- this should probably be in the template
-                if (app.icon)
-                    app.icon = ROOT + app.icon;
-                else
-                    app.icon = '/images/app-generic.png';
-
-                app.screenshot = ROOT + app.screenshot;
-            });
+	renderApps($http, 'builds/' + buildPath, function(apps) {
             $scope.apps = apps;
-        });
+	});
     });
+
+    function compareTaskData(a, b) {
+	var ai = taskNames.indexOf(a['name']);
+	var bi = taskNames.indexOf(b['name']);
+	return ai - bi;
+    }
 
     bgoControllers.controller('ContinuousHomeCtrl', function($scope, $http) {
         var builds = [];
-
-        // Just get the most recent build for now. We need an
-        // API to iterate over all the builds.
-        $http.get(ROOT + 'results/tasks/build/build/meta.json').success(function(data) {
-            data.buildVersion = relpathToVersion(data.buildPath);
-            builds.push(data);
-        });
-        $scope.builds = builds;
 
         $http.get(ROOT + 'autobuilder-status.json').success(function(status) {
             var text;
@@ -98,6 +101,53 @@
 
             $scope.runningState = text;
         });
+
+        var completedTasks = [];
+        taskNames.forEach(function(taskName) {
+	    var href = ROOT + 'results/tasks/' + taskName + '/' + taskName;
+            $http.get(href + '/meta.json').success(function(data) {
+                // Mangle the data a bit so we can render it better
+                data['name'] = taskName;
+		data['version'] = relpathToVersion(data['buildPath']);
+		data['href'] = href;
+                completedTasks.push(data);
+		completedTasks.sort(compareTaskData);
+		$scope[taskName] = data;
+            }).error(function(data, status, headers, config) {
+		data = {};
+		data['name'] = taskName;
+		data['status'] = '(none completed)';
+                completedTasks.push(data);
+		completedTasks.sort(compareTaskData);
+	    });
+        });
+        $scope.completedTasks = completedTasks;
+
+	$http.get(ROOT + '/results/tasks/integrationtest/installed-test-results.js').success(function(data) {
+	    var testname;
+	    var total = 0;
+	    var failed = [];
+	    var successful = [];
+	    var skipped = [];
+	    for (testname in data) {
+		total++;
+		var status = data[testname];
+		if (status == 'success')
+		    successful.push(testname);
+		else if (status == 'failed')
+		    failed.push(testname);
+		else if (status == 'skipped')
+		    skipped.push(testname);
+	    }
+	    $scope.installedTestsTotal = total;
+	    $scope.installedTestsSuccessful = successful;
+	    $scope.installedTestsFailed = failed;
+	    $scope.installedTestsSkipped = skipped;
+	});
+
+	renderApps($http, 'results/tasks/build', function(apps) {
+            $scope.apps = apps;
+	});
     });
 
 })(window);
