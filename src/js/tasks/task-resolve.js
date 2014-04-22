f9331167757673bd0633ada545e80fb2705bf680
Colin Walters <walters@verbum.org>
2014-04-22 16:52:19 -0400

    resolve: Do import GLib
    This regressed with Giovanni's commit to change main.js.

--- src/js/tasks/task-resolve.js
+++ src/js/tasks/task-resolve.js
@@ -15,6 +15,7 @@
 // Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 // Boston, MA 02111-1307, USA.
 
+const GLib = imports.gi.GLib;
 const Gio = imports.gi.Gio;
 const Lang = imports.lang;
 const GSystem = imports.gi.GSystem;
